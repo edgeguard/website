af08d9b64e2e5ce11226a397007b64919c52dfa1
Blake Beckett <edgeguardpodcast@gmail.com>
2018-09-20 23:12:41 -0600

    Updated episode card styles to match mock

--- src/app/episode-card/episode-card.component.ts
+++ src/app/episode-card/episode-card.component.ts
@@ -8,10 +8,23 @@ import { FeedEntry } from '../model/feed-entry';
 })
 export class EpisodeCardComponent implements OnInit {
 	@Input() episodeData: FeedEntry;
+	expanded = false;
+	episodeNumber: number;
+	episodeTitle: string;
 	constructor() { }
 
 	ngOnInit() {
-		
+		this.parseTitle(this.episodeData.title);
+	}
+
+	parseTitle(title: string) {
+		console.log(title);
+		let parts = title.split(' - ');
+		if(parts.length < 2) {
+			parts = title.split(': ');
+		}
+		this.episodeTitle = parts[1].trim();
+		this.episodeNumber = parts[0].match(/\d+/g).map(Number)[0];
 	}
 
 }
